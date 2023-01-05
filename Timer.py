d0725ddb8328ff2d5c4932f28eeceb12ebf8282a
utkarshpandey-koder <116140552+utkarshpandey-koder@users.noreply.github.com>
2023-01-05 10:04:50 +0530

    Update Timer.py

--- Timer.py
+++ Timer.py
@@ -1,3 +1,4 @@
+#Using for loop
 import time
 x=int(input("Enter the Time in Seconds :"))
 for i in range(x , 0 ,-1):
@@ -7,3 +8,19 @@ for i in range(x , 0 ,-1):
   print(f"{hours:02}:{minutes:02}:{seconds:02}")
   time.sleep(1)
 print("Time Up !")
+
+
+
+
+#using while loop
+import time
+x=int(input("Enter the Time in Seconds :"))
+while x>0:
+  seconds=x%60
+  minutes=int(x/60)%60
+  hours=int(x/3600)
+  print(f"{hours:02}:{minutes:02}:{seconds:02}")
+  x-=1
+  time.sleep(1)
+print("Time Up !")
+
